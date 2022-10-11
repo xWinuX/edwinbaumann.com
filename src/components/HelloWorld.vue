0c0218b19f19df164a43f8bde163a36c93a50d3c
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-10-11 19:43:32 +0200

    Created repo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="grid h-screen place-items-center">
+    <div class="h-auto">
+      <h1 class="text-2xl mb-5">{{ msg }}</h1>
+      <div class="text-center">
+        <button class="border-2 rounded p-2" type="button" @click="count++">count is {{ count }}</button>
+      </div>
+    </div>
+  </div>
+</template>
+
