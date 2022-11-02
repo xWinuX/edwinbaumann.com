95aa6121f0b2d97a9327d8ac0a34c8df7e458bc8
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-11-03 00:06:30 +0100

    added animated tailwindcss package

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,9 @@
 /** @type {import("tailwindcss").Config} */
-module.exports = {
-    content: [
-        './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
-    ],
+const withAnimations = require('animated-tailwindcss');
+module.exports = withAnimations({
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {},
     },
     plugins: [],
-};
+});
