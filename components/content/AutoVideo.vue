7e5675547ceab415a7b3e926f392ec04f0d526d6
WinuX <37900754+xWinuX@users.noreply.github.com>
2024-11-10 15:16:28 +0100

    added autoplay video content component

---
+++ components/content/AutoVideo.vue
@@ -0,0 +1,16 @@
+<template>
+  <video autoplay>
+    <source :src="src" type="video/mp4">
+    Your browser does not support videos.
+  </video>
+</template>
+
+<script setup lang="ts">
+
+export interface Props {
+  src: string;
+}
+
+defineProps<Props>();
+
+</script>
