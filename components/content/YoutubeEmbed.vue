6e72492f68d58fbef67dd96a6893e74594ebbd11
WinuX <37900754+xWinuX@users.noreply.github.com>
2024-11-14 00:08:10 +0100

    made youtube embed component dynamic

---
+++ components/content/YoutubeEmbed.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <iframe
+      width="100%"
+      height="600px"
+      :src="videoLink"
+      title="YouTube video player"
+      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  videoId: string;
+}
+
+const props = defineProps<Props>();
+
+const videoLink = computed(() => `https://www.youtube.com/embed/${props.videoId}`);
+
+</script>
