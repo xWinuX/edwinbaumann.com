a75a5b2f05a36ec2451d0d9d9e6d7bcd99e65b4d
WinuX <37900754+xWinuX@users.noreply.github.com>
2024-11-22 01:15:32 +0100

    Added note to youtube embeds that pressing the play button will cause youtube to gather data

--- components/content/YoutubeEmbed.vue
+++ components/content/YoutubeEmbed.vue
@@ -1,36 +1,17 @@
 <template>
-  <div v-if="!declined" class="relative">
-    <div
-      v-if="!consent"
-      class="absolute w-full h-full bg-gray-900/80 backdrop-blur-md"
-    >
-      <div class="absolute left-1/2 top-1/2 text-center w-full" style="transform: translate(-50%, -50%)">
-        <p>
-          By playing the following video you consent to any data that may be collected by YouTube. <br>
-          The video can also be directly watched via this link: <a target="_blank" :href="videoLink">{{ videoLink }}</a>
-        </p>
-        <BaseButton class="bg-green-400" @click="() => consent = true">
-          Consent
-        </BaseButton>
-        <BaseButton class="ml-4 !bg-red-900" @click="() => declined = true">
-          Decline
-        </BaseButton>
-      </div>
-    </div>
+  <div class="relative">
+    <p class="text-center text-red-400 italic">
+      {{ $t('YoutubeEmbedConsent') }}
+    </p>
     <iframe
       width="100%"
       height="600px"
-      :src="noCookieVideoLink"
+      :src="videoLink"
       title="YouTube video player"
       allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       referrerpolicy="strict-origin-when-cross-origin"
       allowfullscreen
     />
-  </div>
-  <div v-else class="pt-4">
-    <p class="text-center text-gray-400">
-      <i>Consent to use this youtube embed was declined, the video can still be accessed directly on YouTube via this link: <a target="_blank" :href="videoLink">{{ videoLink }}</a></i>
-    </p>
   </div>
 </template>
 
@@ -41,9 +22,6 @@ interface Props {
 
 const props = defineProps<Props>();
 
-const noCookieVideoLink = computed(() => `https://www.youtube-nocookie.com/embed/${props.videoId}`);
-const videoLink = computed(() => `https://www.youtube.com/watch?v=${props.videoId}`);
+const videoLink = computed(() => `https://www.youtube-nocookie.com/embed/${props.videoId}`);
 
-const consent = ref(false);
-const declined = ref(false);
 </script>
