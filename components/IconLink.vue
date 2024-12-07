cf4860f76d09e42f2c6b395a24cc89771eb6ad32
WinuX <37900754+xWinuX@users.noreply.github.com>
2024-12-07 04:36:16 +0100

    created icon link component

---
+++ components/IconLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <a class="group" :href="link" target="_blank">
+    <Icon class="transition-transform scale-100 shadow-none group-hover:shadow-glow hover:scale-[110%] svg-shadow-normal" :name="iconName" :size="size" />
+
+  </a>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  link: string;
+  iconName: string;
+  tooltip?: string;
+  size?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+    tooltip: "",
+    size: "3.5rem",
+});
+</script>
