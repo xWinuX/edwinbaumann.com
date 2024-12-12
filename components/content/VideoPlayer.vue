5e3208fc39bf2c6a79fe5acc358dbaea76f9923a
WinuX <37900754+xWinuX@users.noreply.github.com>
2024-12-12 18:57:04 +0100

    added video player component

---
+++ components/content/VideoPlayer.vue
@@ -0,0 +1,15 @@
+<template>
+  <video controls>
+    <source :src="src" type="video/mp4">
+    Your browser does not support videos.
+  </video>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  src: string;
+}
+
+defineProps<Props>();
+
+</script>
