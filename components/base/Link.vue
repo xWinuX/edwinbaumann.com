26bd053db53f88e7b5cac7c4be2cb42c1856f160
WinuX <37900754+xWinuX@users.noreply.github.com>
2024-12-12 18:56:50 +0100

    cleanup link component

--- components/base/Link.vue
+++ components/base/Link.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="inline-block group/link">
-    <NuxtLink class="font-bold gradient-text " :target="target" :to="link" rel="noreferrer noopener">
+    <NuxtLink class="font-bold gradient-text" :target="target" :to="link" rel="noreferrer noopener">
       <slot />
     </NuxtLink>
     <BaseGradientLine class="!h-[0.15rem] transition ease-in-out scale-x-0 group-hover/link:scale-x-100" />
