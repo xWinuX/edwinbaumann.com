e787f1e8ad0287062504b993f7c333bbe5135373
WinuX <37900754+xWinuX@users.noreply.github.com>
2026-05-20 15:23:34 +0200

    updated footer date

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -8,7 +8,7 @@
             <p>
               <b>
                 <span class="inline-block">Edwin Baumann&nbsp;</span>
-                <span class="inline-block">2022 - 2025</span>
+                <span class="inline-block">2022 - 2026</span>
               </b>
             </p>
             <div class="sm:absolute sm:right-0 sm:top-0 sm:pt-5 pt-1 sm:w-auto w-full  mx-auto">
